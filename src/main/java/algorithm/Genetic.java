ba6d23d36918b1160fcc95481d5329b0f12e0f2f
Sander de Winter <sanderdewinter@gamil.com>
2016-05-24 13:17:18 +0200

    Genetic setup

---
+++ src/main/java/algorithm/Genetic.java
@@ -0,0 +1,18 @@
+package algorithm;
+
+public class Genetic {
+
+    private double crossoverRate;
+    private double mutationRate;
+    private boolean elitism;
+    private int populiationSize;
+    private int amountOfIterations;
+
+    public Genetic(double crossoverRate, double mutationRate, boolean elitism, int populiationSize, int amountOfIterations) {
+        this.crossoverRate = crossoverRate;
+        this.mutationRate = mutationRate;
+        this.elitism = elitism;
+        this.populiationSize = populiationSize;
+        this.amountOfIterations = amountOfIterations;
+    }
+}
