c82de11bde9c32dd7052160e1d5b656bf09a78a0
Sander de Winter <sanderdewinter@gmail.com>
2016-06-21 16:17:06 +0200

    stukje

--- src/main/java/assignment2/algorithm/Genetic.java
+++ src/main/java/assignment2/algorithm/Genetic.java
@@ -1,5 +1,13 @@
 package assignment2.algorithm;
 
+import assignment2.models.Individual;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Genetic {
 
     private double crossoverRate;
@@ -7,12 +15,34 @@ public class Genetic {
     private boolean elitism;
     private int populiationSize;
     private int amountOfIterations;
+    private Function<Individual, Double> computeFitness;
 
-    public Genetic(double crossoverRate, double mutationRate, boolean elitism, int populiationSize, int amountOfIterations) {
+    public Genetic(double crossoverRate, double mutationRate, boolean elitism, int populiationSize, int amountOfIterations, Function<Individual, Double> computeFitness) {
         this.crossoverRate = crossoverRate;
         this.mutationRate = mutationRate;
         this.elitism = elitism;
         this.populiationSize = populiationSize;
         this.amountOfIterations = amountOfIterations;
+        this.computeFitness = computeFitness;
+    }
+
+    public void run() {
+        List<Individual> population = initPopulation();
+
+        for (int generation = 0; generation < amountOfIterations; generation++) {
+            List<Double> fitnesses = population.stream().map(individual -> computeFitness.apply(individual)).collect(Collectors.toList());
+
+            List<Individual> nextPopulation = new ArrayList<>();
+        }
+    }
+
+    private List<Individual> initPopulation() {
+        Random r = new Random();
+
+        List<Individual> initialPopulation = new ArrayList<>();
+        for (int i = 0; i < populiationSize; i++) {
+            initialPopulation.add(new Individual(r.nextInt(32)));
+        }
+        return initialPopulation;
     }
 }
--- src/test/java/assignment2/Assignment2Test.java
+++ src/test/java/assignment2/Assignment2Test.java
@@ -1,16 +1,26 @@
 package assignment2;
 
+import assignment2.algorithm.Genetic;
 import assignment2.models.Individual;
+import junit.framework.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.function.Function;
 
 public class Assignment2Test {
 
-    public final Function<Individual, Double> aFakeProblem = b -> {
+    public static final Function<Individual, Double> aFakeProblem = b -> {
         int x = b.getByte().intValue();
         return -1 * Math.pow(x, 2) + 7 * x;
     };
+
+    public static Genetic genetic;
+
+    @BeforeClass
+    public static void init() {
+        genetic = new Genetic(0.5, 0.5, false, 5, 10, aFakeProblem);
+    }
 
     @Test
     public void test() {
@@ -19,6 +29,11 @@ public class Assignment2Test {
 
         System.out.println(individual.getByteString());
         System.out.println(individual2.getByteString());
+        Assert.assertNotNull(individual);
+    }
 
+    @Test
+    public void testGenetic() {
+        genetic.run();
     }
 }
